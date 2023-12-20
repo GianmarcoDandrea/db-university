f04d32fc288285696727230feb44e123c6218e7c
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-20 15:49:18 +0100

    update: add queary.sql to third request

---
+++ query.sql
@@ -0,0 +1,15 @@
+-- 1. Selezionare tutti gli studenti nati nel 1990
+SELECT *
+FROM `students`
+WHERE YEAR(`date_of_birth`) = 1990;
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT `name`, `cfu`
+FROM `courses` 
+WHERE `cfu`> 10;
+
+-- 3.Selezionare tutti gli studenti che hanno più di 30 anni
+
+SELECT `name`, `surname`,`date_of_birth`
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) >= 30;
