43372b1f8445b8a61f2db398adac2684cbd2f288
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-20 16:14:59 +0100

    update: completed exercise

--- query.sql
+++ query.sql
@@ -8,13 +8,32 @@ SELECT `name`, `cfu`
 FROM `courses` 
 WHERE `cfu`> 10;
 
--- 3.Selezionare tutti gli studenti che hanno più di 30 anni
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) >= 30;
 
--- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
 SELECT `name`, `year`, `period`
 FROM `courses`
 WHERE `period` = "I semestre" AND `year` = 1; 
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT `date`, `hour`, `address`, `location`
+FROM `exams`
+WHERE `hour` >= '14:00' AND `date` = '2020-06-20';
+
+-- 6. Selezionare tutti i corsi di laurea magistrale
+SELECT `name` , `level`
+FROM `degrees` 
+WHERE `level` = 'magistrale';
+
+-- 7. Da quanti dipartimenti è composta l'università?
+SELECT  COUNT(*) as `numeber_of_departments`
+FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono?
+SELECT COUNT(*) AS `teachers_without_phone_number`
+FROM `teachers` 
+WHERE `phone` IS NULL;
